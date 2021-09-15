c049b5c1f34984fab18af98d7214bdfc826945a9
FFNG <zedwordmail@gmail.com>
2021-09-15 18:50:27 +0000

    remove Elanthia Online scripts maintained in other Jinx repos to avoid confusion downloading via jinx

--- sync_eol.sh
+++ sync_eol.sh
@@ -2,11 +2,16 @@
 # so we don't have people grabbing older versions
 # another option would be to just ignore them since they are on the EO repo
 # but in the spirit of being a mirror...
-echo "Adding temp remote and checking out EOL scripts dir..."
-git remote add eo https://github.com/elanthia-online/scripts.git
-git fetch eo
-git checkout eo/master -- scripts/
-mv -v scripts/* lib/
-rmdir scripts
-git rm scripts -r > /dev/null
-echo "Removed temp scripts dir..."
+set -o pipefail
+get_repo_manifest_files () {
+	REPO=$1
+	curl -s ${REPO}/manifest.json | jq '.available[] | select (.type == "data" or .type == "script") | .file | split("/")[-1]' -r
+}
+echo "Removing any scripts actively maintained by EO to avoid download conflicts"
+rm lib/.gitignore
+for repo in https://repo.elanthia.online https://extras.repo.elanthia.online ;do
+	get_repo_manifest_files "${repo}" | while read repoFile;do
+		git rm lib/${repoFile}
+		echo $repoFile >> lib/.gitignore
+	done
+done
