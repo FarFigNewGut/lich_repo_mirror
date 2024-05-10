c40d7f5c594295da007d050422a615b479137bcd
FarFigNewGut <50500188+FarFigNewGut@users.noreply.github.com>
2024-05-09 23:25:20 -0500

    rollback. test. exit code is a string

--- lichless_repo/stringproc_notify.rb
+++ lichless_repo/stringproc_notify.rb
@@ -10,7 +10,7 @@ dr_change = ENV['MAPCHANGE_DR']
 debug_out = {"webhooks" => webhooks.nil?, "gs_change" => gs_change, "dr_change" => dr_change}
 puts debug_out
 
-if webhooks && (gs_change == 0 || dr_change == 0)
+if webhooks && (gs_change == "0" || dr_change == "0")
   webhooks.split(',').each { |wh|
     wh.strip!
     uri = URI(wh)
@@ -36,10 +36,10 @@ if webhooks && (gs_change == 0 || dr_change == 0)
         "embeds": []
     }
 
-    if gs_change == 0
+    if gs_change == "0"
         request_body[:embeds].append(gs_embed)
     end
-    if dr_change == 0
+    if dr_change == "0"
         request_body[:embeds].append(dr_embed)
     end
 
